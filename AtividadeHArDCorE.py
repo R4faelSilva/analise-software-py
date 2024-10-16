0360a9f992de1cda3b4e5cdb8b03741724936e70
R4faelSilva <rafaelsilvamachado93@gmail.com>
2024-10-16 19:34:36 -0300

    Teste de Software

--- AtividadeHArDCorE.py
+++ AtividadeHArDCorE.py
@@ -1,23 +1,24 @@
 import random
 lista = [random.randint(1, 100) for _ in range(5)]
 
+
+def adicionarNumero(lista, addNumero):
+    add = int(addNumero)
+    return lista.append(add)
+
+def removerNumero(lista, remNumero):
+    rem = int(remNumero)
+    return lista.remove(rem)
+
+def somaLista(lista):
+    soma = sum(lista)
+    return soma
+    
+def mediaLista(lista):
+    media = sum(lista)/len(lista)
+    return media
+
 def main():
-    def adicionarNumero(lista, addNumero):
-        addNumero = adicionarNumero
-        return lista.append(addNumero)
-
-    def removerNumero(lista, remNumero):
-        if remNumero in lista:
-            lista.remove(remNumero)
-
-    def somaLista(lista):
-        soma = sum(lista)
-        return soma
-        
-    def mediaLista(lista):
-        media = sum(lista)/len(lista)
-        return media
-
 
     while True:
         print("1= Adicionar um número à lista")
@@ -30,11 +31,11 @@ def main():
         try:
             operacao = int(input("Qual operação voce deseja? (1,2,3,4,5,6) "))
             if operacao == 1:
-                addNumero = int(input("Qual o numero que você quer adicionar? "))
-                adicionarNumero(lista, addNumero)
+                add = int(input("Qual o numero que você quer adicionar? "))
+                adicionarNumero(lista, add)
             elif operacao == 2:
-                remNumero = int(input("Qual o numero que você deseja remover? "))
-                removerNumero(lista, remNumero)
+                rem = int(input("Qual o numero que você deseja remover? "))
+                removerNumero(lista, rem)
             elif operacao == 3:
                 print(lista)
             elif operacao == 4:
@@ -46,10 +47,11 @@ def main():
                 break
         except ValueError:
             print("Não é um numero inteiro")
+        except ZeroDivisionError:
+            return print("Não é possível dividir por zero!")
         except Exception as e:
             return e
-        except ZeroDivisionError:
-            print("Não é possível dividir por zero!")
+
 
 if __name__ == '__main__':
     main()
--- testeAtividadeHArDCorE.py
+++ testeAtividadeHArDCorE.py
@@ -1,22 +1,43 @@
 import unittest
-from AtividadeHArDCorE import adicionarNumero,addNumero
+from AtividadeHArDCorE import adicionarNumero,somaLista,removerNumero,mediaLista
 
 class TestAtividade(unittest.TestCase):
+    def setUp(self):
+        self.lista = [10,20,30]
 
-    def test_add_numero(self):
-        self.assertEqual(adicionarNumero(addNumero))
+    def test_add(self):
+        lista = [10,20,30]
+        adicionarNumero(lista,100)
+        self.assertIn(100,lista)
 
-    def removerNumero(lista, remNumero):
-        if remNumero in lista:
-            lista.remove(remNumero)
-
-    def somaLista(lista):
-        soma = sum(lista)
-        return soma
-        
-    def mediaLista(lista):
-        media = sum(lista)/len(lista)
-        return media
+    def test_input(self):
+        with self.assertRaises(ValueError):
+            adicionarNumero(self.lista,'a')
     
+    def test_remove_inexistente(self):
+        with self.assertRaises(ValueError):
+            removerNumero(self.lista, 100)
+
+    def test_remove(self):
+        with self.assertRaises(ValueError):
+            removerNumero(self.lista, 100)
+
+    def test_remove_input(self):
+        with self.assertRaises(ValueError):
+            removerNumero(self.lista,'a')
+
+    def test_soma(self):
+        soma = sum(self.lista)
+        self.assertEqual(somaLista(self.lista),soma)
+
+    def test_media(self):
+        media = sum(self.lista)/len(self.lista)
+        self.assertEqual(mediaLista(self.lista),media)
+    
+    def test_media_zero(self):
+        lista=[]
+        with self.assertRaises(ZeroDivisionError):
+            mediaLista(lista)
+
 if __name__ == '__main__':
-    main()
+    unittest.main()
